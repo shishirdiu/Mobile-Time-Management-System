a54c8e7d7919041261e1a1ad4eca94fc9d8ccb9a
Shishir Sarker <shishirsarker78@gmail.com>
2026-04-15 10:30:33 +0600

    Add custom exceptions for time management

--- TimeManagementSystem.java
+++ TimeManagementSystem.java
@@ -7,3 +7,10 @@ interface Reportable {
 enum AppCategory {
     SOCIAL, GAMING, EDUCATION, OTHER
 }
+class TimeLimitExceededException extends Exception {
+    public TimeLimitExceededException(String msg) { super(msg); }
+}
+
+class InvalidTimeException extends Exception {
+    public InvalidTimeException(String msg) { super(msg); }
+}
